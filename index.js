8b7acef124765d7ae21ff91599520584443080f1
Keerthi-9716 <keerthimaddhu@gmail.com>
2025-10-21 18:15:58 +0800

    added multiplication in feature2

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // index.js
-console.log("Hello, Keerthi! This is your first JavaScript project 🎉");
+console.log("Hello, Keerthi! This is your feature2 branch🎉");
+
+
+let number = 7;
+if (number % 2 === 0) {
+  console.log(number + " is even");
+} else {
+  console.log(number + " is odd");
+}
