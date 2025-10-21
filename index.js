6ad889055ebda790ac5f044add556c88566edc16
Keerthi-9716 <keerthimaddhu@gmail.com>
2025-10-21 18:17:25 +0800

    find largest number

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // index.js
-console.log("Hello, Keerthi! This is your first JavaScript project 🎉");
+console.log("Hello, Keerthi! This is your feature3 branch 🎉");
+
+let a = 5, b = 8, c = 3;
+
+if (a > b && a > c) {
+  console.log("Largest is: " + a);
+} else if (b > c) {
+  console.log("Largest is: " + b);
+} else {
+  console.log("Largest is: " + c);
+}
